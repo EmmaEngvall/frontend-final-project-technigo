0b1acd85bd4a84825c61263a1c8495ddcc0a07ef
Emma Engvall <emma.engvall@ica.se>
2023-06-01 16:00:14 +0200

    added navlinks to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <p>This is the Header!</p>
+    <header>
+      <nav>
+        <NavLink to="/"><p>Home</p></NavLink>
+        <NavLink to="/login"><p>Register/Login</p></NavLink>
+        <NavLink to="/profile"><p>My profile</p></NavLink>
+        <NavLink to="/about"><p>About</p></NavLink>
+        <NavLink to="/contact"><p>Contact</p></NavLink>
+        <NavLink to="/credits"><p>Credits</p></NavLink>
+      </nav>
+    </header>
   )
 }
 
