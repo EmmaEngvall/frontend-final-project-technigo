d19248f774b8e626ff1a37f8e5a5b08071dc48ee
Emma Engvall <emma.engvall@ica.se>
2023-06-06 13:30:31 +0200

    changed text in About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <h1>This is the About page!</h1>
+    <h1>This is the About page! We are two happy students at the Technigo Boot camp.</h1>
   )
 }
 
