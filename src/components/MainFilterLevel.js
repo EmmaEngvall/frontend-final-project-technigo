30a4ee8e62137094562db8ad05a68afc88daa345
Malin <malin@MacBook-Pro-4.local>
2023-06-10 21:09:14 +0200

    added filtering for location on Main it should now work together with level filtering

--- src/components/MainFilterLevel.js
+++ src/components/MainFilterLevel.js
@@ -1,39 +1,83 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import surfPosts from 'reducers/surfPosts';
 
 const MainFilterLevel = () => {
   const [selectedLevel, setSelectedLevel] = useState('all');
+  const [selectedLocation, setSelectedLocation] = useState('all');
   const dispatch = useDispatch();
-  const orignalItems = useSelector((store) => store.surfPosts.allItems);
+  const originalItems = useSelector((store) => store.surfPosts.allItems);
+  const filteredItemsState = useSelector((store) => store.surfPosts.filteredItems);
 
-  const filterPostByLevel = (level) => {
-    let filteredItems = [];
+  const filterPostByLevel = useCallback((level, location) => {
+    let filteredItems = originalItems;
 
-    if (level === 'beginner' || level === 'intermediate' || level === 'advanced') {
-      filteredItems = orignalItems.filter((item) => item.level === level);
-    } else {
-      filteredItems = orignalItems;
+    if (level !== 'all') {
+      filteredItems = filteredItems.filter((item) => item.level === level);
+    }
+    if (location !== 'all') {
+      filteredItems = filteredItems.filter((item) => item.location === location);
     }
 
     dispatch(surfPosts.actions.setFilteredItems(filteredItems));
-  }
+  }, [dispatch, originalItems]);
 
-  const handleFilterChanges = (event) => {
+  useEffect(() => {
+    filterPostByLevel(selectedLevel, selectedLocation);
+  }, [selectedLevel, selectedLocation, filterPostByLevel])
+
+  const handleLevelChanges = (event) => {
     const level = event.target.value;
     setSelectedLevel(level);
 
-    filterPostByLevel(level);
+    if (level === 'all' && selectedLocation === 'all') {
+      dispatch(surfPosts.actions.setFilteredItems(originalItems))
+    }
   }
 
+  const handleLocationChanges = (event) => {
+    const location = event.target.value;
+    setSelectedLocation(location);
+
+    if (location === 'all' && selectedLevel === 'all') {
+      dispatch(surfPosts.actions.setFilteredItems(originalItems))
+    }
+  }
+
+  // extract uniqueLocations from filteredItems, we map and use the Set object to elimante dulicates
+  // Spread operator to convert the Set object back to an array
+  const uniqueLocations = [...new Set(filteredItemsState.map((items) => items.location))];
+  const uniqueLevels = [...new Set(filteredItemsState.map((items) => items.level))];
+
   return (
-    <select onChange={handleFilterChanges} value={selectedLevel}>
-      <option value="" disabled>Sort on level</option>
-      <option value="beginner">Beginner</option>
-      <option value="intermediate">Intermediate</option>
-      <option value="advanced">Advanced</option>
-      <option value="all">All levels</option>
-    </select>
+    <div>
+      <div>
+        <label htmlFor="levelSelect">Sort on level
+          <select onChange={handleLevelChanges} value={selectedLevel}>
+            <option value="all" disabled>Sort on level</option>
+            <option value="all">All levels</option>
+            {uniqueLevels.map((level) => {
+              return (
+                <option key={level} value={level}>{level}</option>
+              )
+            })}
+          </select>
+        </label>
+      </div>
+      <div>
+        <label htmlFor="locationSelect">Sort on location
+          <select onChange={handleLocationChanges} value={selectedLocation}>
+            <option value="all" disabled>Filter by location</option>
+            <option value="all">Clear filter</option>
+            {uniqueLocations.map((location) => {
+              return (
+                <option key={location} value={location}>{location}</option>
+              )
+            })}
+          </select>
+        </label>
+      </div>
+    </div>
   )
 }
 
