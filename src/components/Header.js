c5047df8b2b26686971dd1e0d54535dbd25be4d9
Malin <malin@MacBook-Pro-4.local>
2023-06-11 18:13:15 +0200

    started on navbar, needs other img for the buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import logo from '../images/logo_white_new.jpg';
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
@@ -8,14 +9,15 @@ const Header = () => {
       <div className="logo">
         <img className="logo" src={logo} alt="logo" />
       </div>
-      <nav>
+      <Navbar />
+      {/* <nav>
         <NavLink to="/"><p>Home</p></NavLink>
         <NavLink to="/login"><p>Register/Login</p></NavLink>
         <NavLink to="/profile"><p>My profile</p></NavLink>
         <NavLink to="/about"><p>About</p></NavLink>
         <NavLink to="/contact"><p>Contact</p></NavLink>
         <NavLink to="/credits"><p>Credits</p></NavLink>
-      </nav>
+      </nav> */}
     </header>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { StyledNavBar, StyledNavLink, UlElements, LiElements, ElementP, ToggleButton, NavWrapper } from 'styled/NavbarStyled';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // useRef to be able to connect to the DOM element and detect if mouseclick/scroll
+  // is within navbar or outside
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isOpen && navRef.current && !navRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    const handleScrollOutside = () => {
+      setIsOpen(false);
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener('scroll', handleScrollOutside);
+
+    // Cleanup function remove eventlisteners,executed when component is unmounted or re-rendered
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScrollOutside);
+    }
+  }, [isOpen]);
+
+  console.log(isOpen)
+  return (
+    <StyledNavBar>
+      {isOpen && <ToggleButton onClick={() => setIsOpen(false)}><p>X</p></ToggleButton>}
+      {!isOpen && <ToggleButton onClick={() => setIsOpen(true)}><p>🟰</p></ToggleButton>}
+      <NavWrapper ref={navRef} isOpen={isOpen}>
+        <nav>
+          <UlElements>
+            <LiElements>
+              <StyledNavLink to="/"><ElementP>Home</ElementP></StyledNavLink>
+            </LiElements>
+            <LiElements>
+              <StyledNavLink to="/login"><ElementP>Register/Login</ElementP></StyledNavLink>
+            </LiElements>
+            <LiElements>
+              <StyledNavLink to="/profile"><ElementP>My profile</ElementP></StyledNavLink>
+            </LiElements>
+            <LiElements>
+              <StyledNavLink to="/about"><ElementP>About</ElementP></StyledNavLink>
+            </LiElements>
+            <LiElements>
+              <StyledNavLink to="/contact"><ElementP>Contact</ElementP></StyledNavLink>
+            </LiElements>
+            <LiElements>
+              <StyledNavLink to="/credits"><ElementP>Credits</ElementP></StyledNavLink>
+            </LiElements>
+          </UlElements>
+        </nav>
+      </NavWrapper>
+    </StyledNavBar>
+  );
+};
+
+export default Navbar
