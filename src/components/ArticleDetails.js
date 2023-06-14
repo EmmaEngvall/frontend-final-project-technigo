cd552bc4a8d3018add4eaa98d311932a2898507e
Emma Engvall <emma.engvall@ica.se>
2023-06-14 11:07:24 +0200

    fixed so that images show in articles

--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Introduction } from 'styled/ArticleDetailsStyled';
-import jsonData from './data.json';
+import jsonData from '../utils/data.json';
+import imgVarberg from '../images/article_Varberg.jpg';
+import imgLofoten from '../images/article_Lofoten.jpg';
+import imgCornwall from '../images/article_Cornwall.jpg';
+import imgBH from '../images/article_BH.jpg';
 
 console.log(jsonData);
 
@@ -9,6 +13,17 @@ const ArticleDetails = () => {
   const { articleId } = useParams()
   const [article, setArticle] = useState({})
   console.log(articleId)
+  let imgSrc;
+
+  if (articleId === '01') {
+    imgSrc = imgVarberg
+  } else if (articleId === '02') {
+    imgSrc = imgLofoten
+  } else if (articleId === '03') {
+    imgSrc = imgCornwall
+  } else {
+    imgSrc = imgBH
+  }
 
   useEffect(() => {
     const articleData = jsonData.find((item) => item.id === articleId);
@@ -20,7 +35,9 @@ const ArticleDetails = () => {
   return (
     <div>
       <div key={article.id}>
+        <p>{article.date}</p>
         <h1>{article.title}</h1>
+        <img className="article_images" src={imgSrc} alt="article destination" />
         <Introduction>{article.intro}</Introduction>
         <p>{article.body}</p>
         <p>{article.conclusion}</p>
